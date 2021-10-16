a0cdf2ce59da8785c9521f6581bd54d06008cbe8
GusttavoCastilho <gustavocvalenciano@gmail.com>
2021-10-16 19:38:57 -0300

    chore: Remove firebase config 🔧

--- firebase-config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyDrjaWfwfgzxWV7J3z13DdKZrp1FYRkdrQ",
-    authDomain: "nectar-7c5bf.firebaseapp.com",
-    projectId: "nectar-7c5bf",
-    storageBucket: "nectar-7c5bf.appspot.com",
-    messagingSenderId: "1020916271789",
-    appId: "1:1020916271789:web:f32b87b5ff91727a455424"
-  };
