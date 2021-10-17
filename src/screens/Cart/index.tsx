34e019cadbd22b06d730f37f99c30aefe8dea8c3
GusttavoCastilho <gustavocvalenciano@gmail.com>
2021-10-17 13:53:04 -0300

    chore: Refactor cart screen 🔧

--- src/screens/Cart/index.tsx
+++ src/screens/Cart/index.tsx
@@ -1,21 +1,28 @@
-import React from 'react'
+import React from "react";
 
-import {View, Text} from 'react-native'
-import CartCard from '../../components/CartCard'
-import * as S from './styles'
-import { items } from '../../components/CartCard/mock/items'
+import CartCard from "../../components/CartCard";
+import * as S from "./styles";
+import { items } from "../../components/CartCard/mock/items";
 
 const Cart: React.FC = () => {
-    return (
-        <S.Container>
-            <S.Header>
-                <S.TitleHeader>My Cart</S.TitleHeader>
-            </S.Header>
-            {items.map(( item, index ) => {
-                return <CartCard image={item.image} title={item.title} description={item.description} price={item.price} />
-            })}
-        </S.Container>
-    )
-}
+  return (
+    <S.Container>
+      <S.Header>
+        <S.TitleHeader>My Cart</S.TitleHeader>
+      </S.Header>
+      {items.map((item, index) => {
+        return (
+          <CartCard
+            key={index}
+            image={item.image}
+            title={item.title}
+            description={item.description}
+            price={item.price}
+          />
+        );
+      })}
+    </S.Container>
+  );
+};
 
-export default Cart
+export default Cart;
